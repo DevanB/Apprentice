bdd6685f3d6324c727c8e365a0fa92911bf52bc3
Devan Beitel <devan@devanb.us>
2018-07-08 13:05:01 -0400

    Adds UIFont types and walkthrough card assets

---
+++ Apprentice/UIFont+Additions.swift
@@ -0,0 +1,33 @@
+//
+//  UIFont+Additions.swift
+//  sotmk-app
+//
+//  Generated on Zeplin. (7/6/2018).
+//  Copyright (c) 2018 __MyCompanyName__. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+
+  class var subtitle: UIFont {
+    return UIFont.systemFont(ofSize: 18.0, weight: .medium)
+  }
+
+  class var link: UIFont {
+    return UIFont.systemFont(ofSize: 18.0, weight: .bold)
+  }
+
+  class var largeHeading: UIFont {
+    return UIFont.systemFont(ofSize: 36.0, weight: .bold)
+  }
+
+  class var heading: UIFont {
+    return UIFont.systemFont(ofSize: 30.0, weight: .bold)
+  }
+
+  class var body: UIFont {
+    return UIFont.systemFont(ofSize: 17.0, weight: .regular)
+  }
+
+}
